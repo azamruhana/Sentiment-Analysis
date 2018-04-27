d00a1362a2ccc4f14655d43f607318abe688422a
Ruhana <razam@purdue.edu>
2018-04-26 22:53:07 -0400

    first submission version of code

--- kaggle.py
+++ kaggle.py
@@ -20,7 +20,6 @@ def nbc(data):
 	for i in range (0, len(data)):
 		data[i][1] = data[i][1].split() # seperate each word
 		for j in range (0, len(data[i][1])): #insert each word from one data set
-
 			if data[i][1][j] not in words:
 				words[data[i][1][j]] = 0
 				for x in range (0,len(countList)):
@@ -33,11 +32,6 @@ def nbc(data):
 			words[data[i][1][j]] += 1
 			totalList[out] += 1
 			
-
-	# print words
-	# print 
-	# printDict(countList)
-	# print totalList
 	return words, countList, totalList
 	
 
@@ -61,16 +55,29 @@ def accuracy(data,words, countList, totalList):
 	print 'ERROR: ', (error*1.0/len(data))
 	print 'Accuracy: ', (1 - (error*1.0/len(data)))
 
+def testPredict(data,words, countList, totalList):
+	output = pd.DataFrame(index=range(0,len(data)), columns=['id', 'sentiment'])
+	for i in range(0, len(data)):
+		prediction = predict(data[i][1], words, countList, totalList)
+		bestClass = prediction.index(max(prediction))
+		output.loc[i, 'id'] = data[i][0]
+		output.loc[i, 'sentiment'] = bestClass
+	print output
+	print
+	print len(output)
+	output.to_csv('results.csv', index = False)
+
 
 def predict(dataPoint,words, countList, totalList):
 	predict = [1.0] * 5
+
 	for i in range(0, len(dataPoint)): # i is each word
 		if dataPoint[i] in words:
 			for j in range (0, len(predict)):
 				predict[j] = predict[j] * ((1.0*countList[j][dataPoint[i]] + 1)/(words[dataPoint[i]]+ len(words)))
 		else:
 			for j in range (0, len(predict)):
-				predict[j] = predict[j] * ((1.0)/(words[dataPoint[i]]+ len(words)))
+				predict[j] = predict[j] * ((1.0)/len(words))
 	return predict
 
 def main():
@@ -78,7 +85,7 @@ def main():
 	testFile = sys.argv[2]
 
 	data = pd.read_csv(trainingFile, sep=',' , quotechar='"', header=0, engine='python')
-	testData = pd.read_csv(trainingFile, sep=',' , quotechar='"', header=0, engine='python')
+	testData = pd.read_csv(testFile, sep=',' , quotechar='"', header=0, engine='python')
 	data = data.as_matrix()
 	testData = testData.as_matrix()
 
@@ -87,7 +94,7 @@ def main():
 
 	for i in range (0, len(testData)):
 		testData[i][1] = testData[i][1].split()
-	accuracy(testData, out[0], out[1], out[2])
+	testPredict(testData, out[0], out[1], out[2])
 	
 
 
